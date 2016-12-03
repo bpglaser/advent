06d42d2912dd6c848eb13708524313aab6af35a7
bpglaser <bpglaser@gmail.com>
2016-12-03 09:32:13 -0500

    Submitted day 03 part 01

---
+++ rust/day03_part01/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::stdin;
+
+fn main() {
+    let count = read_lines().iter().filter(|s| is_valid_triangle(&s)).count();
+    println!("Valid triangle count: {}", count);
+}
+
+fn read_lines() -> Vec<String> {
+    let mut lines = vec![];
+    loop {
+        let mut buf = String::new();
+        stdin().read_line(&mut buf).expect("error reading stdin");
+        if buf.trim().is_empty() {
+            break;
+        } else {
+            lines.push(buf.clone());
+            buf.clear();
+        }
+    }
+    lines
+}
+
+fn is_valid_triangle(input: &str) -> bool {
+    let mut sides: Vec<isize> = input.split_whitespace().map(|s| s.parse().expect("error parsing number")).collect();
+    sides.sort();
+    (sides[0] + sides[1]) > sides[2]
+}
