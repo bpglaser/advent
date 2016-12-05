042dcf2d0c2440e47e4f22d152a8a1615dab8032
bpglaser <bpglaser@gmail.com>
2016-12-05 17:05:54 -0500

    Submitted day 05 part 01

--- src/day05_part01/day05_part01.lua
+++ src/day05_part01/day05_part01.lua
@@ -6,13 +6,19 @@ assert(input, "no puzzle input passed")
 local i = 0
 local password = ""
 
-while string.len(password) < 8 do
+while string.len(password) <= 8 do
     local tohash = input .. i
     local sum = md5.sumhexa(tohash)
-    print(tohash .. "\t=>\t" .. sum .. "\t[" .. password .. "]")
+
     if string.sub(sum, 1, 5) == "00000" then
         password = password .. string.sub(sum, 6, 6)
+        print("found: " .. tohash .. "\t=>\t" .. sum .. "\t[" .. password .. "]")
     end
+
+    if i % 50000 == 0 then
+        print("progress: " .. tohash)
+    end
+
     i = i + 1
 end
 
