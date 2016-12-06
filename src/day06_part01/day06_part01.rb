b9120fa310b905ba4e14996ce68c2388df3d24c1
bpglaser <bpglaser@gmail.com>
2016-12-06 11:43:26 -0500

    Submitted day 06 part 01

---
+++ src/day06_part01/day06_part01.rb
@@ -0,0 +1,10 @@
+lines = File.open("../../input/day06_part01.txt").read().lines().each { |line| line.strip! }
+result = ""
+(0..lines[0].length - 1).each { |column|
+    totals = Hash.new(0)
+    lines.each { |line|
+        totals[line[column]] += 1
+    }
+    result += totals.to_a().sort_by { |obj| obj[1] }.reverse[0][0]
+}
+puts result
