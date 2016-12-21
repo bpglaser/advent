e21a0d1cb836dbe0509546b754006a54bb79af4b
bpglaser <bpglaser@gmail.com>
2016-12-21 14:02:10 -0500

    Work on day 18 part 02

---
+++ src/day18_part02/src/main.rs
@@ -0,0 +1,58 @@
+use std::env::args;
+
+fn main() {
+    let (row_count, mut prev_row) = get_input();
+
+    let mut count = count_safe_tiles(&prev_row);
+
+    for _ in 1..row_count {
+        let next = gen_row(&prev_row);
+        count += count_safe_tiles(&next);
+        prev_row = next;
+    }
+
+    println!("Safe tiles: {}", count);
+}
+
+fn gen_row(previous: &str) -> String {
+    let mut row = String::new();
+
+    for (i, center) in previous.char_indices() {
+        let mut left = '.';
+        if i as isize - 1 >= 0 {
+        left = previous.chars().nth(i - 1).unwrap_or('.');
+        }
+        let right = previous.chars().nth(i + 1).unwrap_or('.');
+
+        if left == '^' && center == '^' && right == '.' {
+            row.push('^');
+        } else if left == '.' && center == '^' && right == '^' {
+            row.push('^');
+        } else if left == '^' && center == '.' && right == '.' {
+            row.push('^');
+        } else if left == '.' && center == '.' && right == '^' {
+            row.push('^');
+        } else {
+            row.push('.');
+        }
+    }
+
+    row
+}
+
+fn count_safe_tiles(row: &str) -> usize {
+    let mut count = 0;
+
+    for c in row.chars() {
+        if c == '.' {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn get_input() -> (usize, String) {
+    let args: Vec<_> = args().skip(1).collect();
+    (args[0].parse().unwrap(), args[1].to_owned())
+}
