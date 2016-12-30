b4c57e6456e221d7f671d66af408916a3e1e3ed2
bpglaser <bpglaser@gmail.com>
2016-12-30 17:36:37 -0500

    Submitted 2015 day 07 part 01

--- 2015/src/day07_part01/src/main.rs
+++ 2015/src/day07_part01/src/main.rs
@@ -6,17 +6,23 @@ use std::io::Read;
 use Operation::*;
 
 fn main() {
-    let mut wires: HashMap<&str, Option<u16>> = HashMap::new();
-    let mut gates: Vec<Gate> = load_lines().iter().map(|s| Gate::parse(&s)).collect();
+    let mut stack: Vec<_> = load_lines().iter().map(|s| Gate::parse(&s)).collect();
+    let mut wires = HashMap::new();
 
-    let mut to_remove = vec![];
-    for gate in gates
+    while stack.len() > 0 {
+        let gate = stack.remove(0);
+        if !gate.evaluate(&mut wires) {
+            stack.push(gate);
+        }
+    }
 
-    let mut wires: Vec<(&str, Option<u16>)> = wires.into_iter().collect();
-    wires.sort_by_key(|&(k, _)| k);
-    for (label, value) in wires {
+    let mut results: Vec<_> = wires.iter().collect();
+    results.sort_by_key(|pair| pair.0);
+    for &(label, value) in results.iter() {
         println!("[{}]\t=>\t{:?}", label, value);
     }
+
+    println!("Answer: {}", wires.get("a").unwrap().unwrap());
 }
 
 #[derive(PartialEq)]
@@ -26,7 +32,7 @@ enum Operation {
     OR,
     LSHIFT,
     RSHIFT,
-    NOOP,
+    INSERT,
 }
 
 struct Gate {
@@ -38,7 +44,7 @@ struct Gate {
 
 impl Gate {
     fn parse(line: &str) -> Gate {
-        let words: Vec<&str> = line.split_whitespace().collect();
+        let words: Vec<_> = line.split_whitespace().collect();
 
         let operation;
         let input_a;
@@ -56,17 +62,80 @@ impl Gate {
                 "OR" => OR,
                 "LSHIFT" => LSHIFT,
                 "RSHIFT" => RSHIFT,
-                "->" => NOOP,
+                "->" => INSERT,
                 _ => panic!("Invalid operator"),
             };
 
-            if operation != NOOP {
+            if operation != INSERT {
                 input_b = Some(words[2].to_owned());
             }
 
         }
 
         Gate { input_a: input_a, input_b: input_b, operation: operation, output: output }
+    }
+
+    fn evaluate(&self, wires: &mut HashMap<String, Option<u16>>) -> bool {
+        let input_a = get_value(&self.input_a, &wires);
+        let input_b = match self.input_b {
+            Some(ref s) => get_value(s, &wires),
+            None => None,
+        };
+
+        let result = match self.operation {
+            NOT => {
+                match input_a {
+                    Some(n) => Some(!n),
+                    None => None,
+                }
+            }
+            AND => {
+                match (input_a, input_b) {
+                    (Some(a), Some(b)) => Some(a & b),
+                    _ => None,
+                }
+            }
+            OR => {
+                match (input_a, input_b) {
+                    (Some(a), Some(b)) => Some(a | b),
+                    _ => None,
+                }
+            }
+            LSHIFT => {
+                match (input_a, input_b) {
+                    (Some(a), Some(b)) => Some(a << b),
+                    _ => None,
+                }
+            }
+            RSHIFT => {
+                match (input_a, input_b) {
+                    (Some(a), Some(b)) => Some(a >> b),
+                    _ => None,
+                }
+            }
+            INSERT => input_a,
+        };
+
+        match result {
+            Some(n) => {
+                let entry = wires.entry(self.output.to_owned()).or_insert(None);
+                *entry = Some(n);
+                true
+            }
+            None => false,
+        }
+    }
+}
+
+fn get_value(s: &str, wires: &HashMap<String, Option<u16>>) -> Option<u16> {
+    match s.parse() {
+        Ok(n) => Some(n),
+        Err(_) => {
+            match wires.get(s) {
+                Some(&Some(n)) => Some(n),
+                _ => None,
+            }
+        }
     }
 }
 
