a318fb4fd40ad6937069289e9794c5ee23767571
bpglaser <bpglaser@gmail.com>
2017-12-03 13:48:14 -0500

    Day 03 part 01 completed

--- 2017/src/day03_part01/src/main.rs
+++ 2017/src/day03_part01/src/main.rs
@@ -1,43 +1,58 @@
 use std::collections::HashMap;
+use std::env::args;
 
 fn main() {
-    let grid = generate_grid(25);
-    println!("{:?}", grid);
+    let input = args().nth(1).unwrap().parse().unwrap();
+    let answer = find_answer(input);
+    println!("answer: {}", answer);
 }
 
-fn generate_grid(target: usize) -> HashMap<u32, (i32, i32)> {
+fn find_answer(target: usize) -> u32 {
     let mut grid = HashMap::with_capacity(target);
-    grid.insert(1, (0, 0));
+    grid.insert((0, 0), 1);
 
-    let mut x = 1;
-    let mut y = 0;
+    let mut n = 2;
+    let mut x = 1i32;
+    let mut y = 0i32;
     let mut side = 0;
-    let mut side_len = 3;
-    let mut count = 1;
 
-    for n in 2..target + 1 {
-        grid.insert(n as u32, (x, y));
-        println!("[{}]\t({}, {}) => side={}\tside_len={}\tcount={}", n, x, y, side, side_len, count);
+    loop {
+        if n == target {
+            return (x.abs() + y.abs()) as u32;
+        }
+        grid.insert((x, y), n);
 
-        count += 1;
-        if count == side_len - 1 {
+        let left = match side {
+            0 => (x - 1, y),
+            1 => (x, y + 1),
+            2 => (x + 1, y),
+            3 => (x, y - 1),
+            _ => unreachable!(),
+        };
+
+        if grid.contains_key(&left) {
+            // move forward
+            match side {
+                0 => y -= 1,
+                1 => x -= 1,
+                2 => y += 1,
+                3 => x += 1,
+                _ => unreachable!(),
+            }
+        } else {
+            // turn left
             side = (side + 1) % 4;
-            if side == 0 {
-                count = 1;
-                side_len += 2;
-            } else {
-                count = 0;
+
+            // move forward
+            match side {
+                0 => y -= 1,
+                1 => x -= 1,
+                2 => y += 1,
+                3 => x += 1,
+                _ => unreachable!(),
             }
         }
 
-        match side {
-            0 => y -= 1,
-            1 => x -= 1,
-            2 => y += 1,
-            3 => x += 1,
-            _ => unreachable!(),
-        }
+        n += 1;
     }
-
-    grid
 }
