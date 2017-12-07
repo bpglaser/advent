e6f7821b53ae35daafb54601fbd6c2ef5b5f0fb3
bpglaser <bpglaser@gmail.com>
2017-12-07 15:37:08 -0500

    Day 07 part 01 completed

--- 2017/src/day07_part01/src/main.rs
+++ 2017/src/day07_part01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
@@ -5,23 +6,36 @@ use std::str::FromStr;
 
 fn main() {
     let path = args().nth(1).unwrap();
-    let mut nodes = load_input(&path);
+    let nodes = load_input(&path);
+
+    let mut parent_map: HashMap<String, u32> = HashMap::new();
+    for node in &nodes {
+        for child in &node.children {
+            if !parent_map.contains_key(child) {
+                parent_map.insert(child.clone(), 0);
+            }
+            *parent_map.get_mut(child).unwrap() += 1;
+        }
+    }
+
+    let answer = nodes.iter()
+        .find(|node| !parent_map.contains_key(&node.name))
+        .unwrap();
+    println!("Root node: {:?}", answer);
 }
 
-fn load_input(path: &str) -> Node {
+fn load_input(path: &str) -> Vec<Node> {
     let mut file = File::open(path).unwrap();
     let mut buf = String::new();
     file.read_to_string(&mut buf).unwrap();
-
-    let mut orphans = vec![];
-    for line in buf.lines().map(|s| s.parse().unwrap()) {
-    }
+    buf.lines().map(|s| s.parse().unwrap()).collect()
 }
 
+#[derive(Debug)]
 struct Node {
     name: String,
     weight: u32,
-    children: Vec<IncompleteNode>,
+    children: Vec<String>,
 }
 
 impl FromStr for Node {
@@ -38,7 +52,6 @@ impl FromStr for Node {
             .into_iter()
             .skip(3)
             .map(remove_trailing_comma)
-            .map(|s| IncompleteNode::Placeholder(s))
             .collect();
 
         Ok(Node {
@@ -55,9 +68,4 @@ fn remove_parens(s: &str) -> String {
 
 fn remove_trailing_comma(s: &str) -> String {
     s.chars().take_while(|c| c != &',').collect()
-}
-
-enum IncompleteNode {
-    Placeholder(String),
-    Complete(Node),
 }
