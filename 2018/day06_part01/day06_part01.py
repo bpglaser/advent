7a7c69ee66caedecf6b411bc54e761931a4f5fd3
bpglaser <bpglaser@gmail.com>
2018-12-06 20:33:18 -0500

    Day 06 part 01 complete

--- 2018/day06_part01/day06_part01.py
+++ 2018/day06_part01/day06_part01.py
@@ -1,71 +1,57 @@
-from collections import defaultdict, deque
-import string
 from sys import argv
 
 
 def main():
     points = load_input(argv[1])
     bounds = find_bounds(points)
-    names = {p: c for p, c in zip(points, string.ascii_letters)}
-
-    grids = {p: fill(p, bounds) for p in points}
-
-    master_grid = {}
-    for 
-
-
-def fill(parent, bounds):
     minx, miny, maxx, maxy = bounds
+
     grid = {}
-    for x in range(minx, maxx + 1):
-        for y in range(miny, maxy + 1):
-            grid[(x, y)] = manhattan_distance((x, y), parent)
-    return grid
+    for y in range(miny, maxy + 1):
+        for x in range(minx, maxx + 1):
+            closest_point, closest_point_dist = None, None
+            for point in points:
+                dist = manhattan_distance((x, y), point)
+                if closest_point_dist is None or dist < closest_point_dist:
+                    closest_point = point
+                    closest_point_dist = dist
+                elif dist == closest_point_dist:
+                    closest_point = None
+            grid[(x, y)] = closest_point
+
+    counts = {}
+    inf = set()
+    for point, parent in grid.items():
+        x, y = point
+        if parent is None or parent in inf:
+            continue
+        if x == minx or x == maxx or y == miny or y == maxy:
+            inf.add(parent)
+            if parent in counts:
+                del counts[parent]
+            continue
+        if parent not in counts:
+            counts[parent] = 0
+        counts[parent] += 1
+    print(max(counts.values()))
 
 
 def manhattan_distance(a, b):
     x1, y1 = a
     x2, y2 = b
-    return abs(x1 - x2) + abs(y1 - y1)
-
-
-def print_grid(grid, bounds, names):
-    minx, miny, maxx, maxy = bounds
-    for y in range(miny, maxy):
-        for x in range(minx, maxx):
-            if (x, y) in names:
-                print(names[(x, y)].upper(), end='')
-                continue
-            if (x, y) not in grid:
-                print('_', end='')
-            elif grid[(x, y)] is None:
-                print('.', end='')
-            else:
-                print(names[grid[(x, y)]], end='')
-        print()
-
-
-def outofbounds(point, bounds):
-    x, y = point
-    minx, miny, maxx, maxy = bounds
-    return x < minx or x > maxx or y < miny or y > maxy
-
-
-def neighbors(point):
-    x, y = point
-    # up, down left right
-    return (x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 def find_bounds(points):
+    safety = 100
     minx, miny, maxx, maxy = points[0][0], points[0][0], points[0][1], points[0][1]
 
     for p in points[1:]:
         x, y = p
-        minx = min(minx, x - 1)
-        miny = min(miny, y - 1)
-        maxx = max(maxx, x + 1)
-        maxy = max(maxy, y + 1)
+        minx = min(minx, x - safety)
+        miny = min(miny, y - safety)
+        maxx = max(maxx, x + safety)
+        maxy = max(maxy, y + safety)
 
     return minx, miny, maxx, maxy
 
