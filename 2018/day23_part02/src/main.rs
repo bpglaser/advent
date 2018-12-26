915a512677cd9a1587ac48705255a8e032c114c9
bpglaser <bpglaser@gmail.com>
2018-12-26 10:53:04 -0500

    Day 23 part 02 fixed

--- 2018/day23_part02/src/main.rs
+++ 2018/day23_part02/src/main.rs
@@ -4,32 +4,79 @@ use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 
-use cassowary::*;
-use cassowary::strength::*;
-
-// 240 too low
 fn main() {
     let path = args().nth(1).expect("input path");
-    let mut nanobots = parse_input(&path);
+    let nanobots = parse_input(&path);
 
-    let mut solver = Solver::new();
+    let candidates: Vec<_> = nanobots
+        .iter()
+        .flat_map(|nanobot| {
+            vec![
+                (nanobot.x, nanobot.y, nanobot.z - nanobot.r),
+                (nanobot.x, nanobot.y, nanobot.z + nanobot.r),
+                (nanobot.x, nanobot.y - nanobot.r, nanobot.z),
+                (nanobot.x, nanobot.y + nanobot.r, nanobot.z),
+                (nanobot.x - nanobot.r, nanobot.y, nanobot.z),
+                (nanobot.x + nanobot.r, nanobot.y, nanobot.z),
+            ]
+        })
+        .map(|pos| (pos, test_pos(pos, &nanobots)))
+        .collect();
 
-    let x = Variable::new();
-    let y = Variable::new();
-    let z = Variable::new();
+    let mut max_n = *candidates.iter().map(|(_pos, n)| n).max().unwrap();
+    let best: Vec<_> = candidates.iter().filter(|&&(_pos, n)| n == max_n).collect();
 
-    for bot in &nanobots {
-        solver.add_constraint(
-            (x - bot.x).abs() + (y - bot.y).abs() + (z - bot.z).abs() |LE(REQUIRED)| bot.r
-        );
+    let mut best_pos = best[0].0;
+
+    'step: loop {
+        for candidate in adjacent_candidates(best_pos) {
+            let n = test_pos(candidate, &nanobots);
+            if n >= max_n {
+                if n > max_n {
+                    max_n = n;
+                }
+                best_pos = candidate;
+                continue 'step;
+            }
+        }
+        break;
     }
+
+    println!("{}", best_pos.0.abs() + best_pos.1.abs() + best_pos.2.abs());
 }
 
-fn parse_input(path: &str) -> Vec<NanoBot> {
-    let mut buf = String::new();
-    let mut file = File::open(path).unwrap();
-    file.read_to_string(&mut buf).unwrap();
-    buf.lines().map(|line| line.parse().unwrap()).collect()
+fn adjacent_candidates(pos: (i32, i32, i32)) -> Vec<(i32, i32, i32)> {
+    let mut result = Vec::new();
+    if pos.0 != 0 {
+        result.push((pos.0 - pos.0.signum(), pos.1, pos.2));
+    }
+    if pos.1 != 0 {
+        result.push((pos.0, pos.1 - pos.1.signum(), pos.2));
+    }
+    if pos.2 != 0 {
+        result.push((pos.0, pos.1, pos.2 - pos.2.signum()));
+    }
+    if pos.0 != 0 && pos.1 != 0 {
+        result.push((pos.0 - pos.0.signum(), pos.1 - pos.1.signum(), pos.2));
+    }
+    if pos.1 != 0 && pos.2 != 0 {
+        result.push((pos.0, pos.1 - pos.1.signum(), pos.2 - pos.2.signum()));
+    }
+    if pos.2 != 0 && pos.0 != 0 {
+        result.push((pos.0 - pos.0.signum(), pos.1, pos.2 - pos.2.signum()));
+    }
+    result
+}
+
+fn test_pos(pos: (i32, i32, i32), nanobots: &[NanoBot]) -> usize {
+    nanobots
+        .iter()
+        .filter(|nanobot| {
+            let (dx, dy, dz) = (nanobot.x - pos.0, nanobot.y - pos.1, nanobot.z - pos.2);
+            let dist = dx.abs() + dy.abs() + dz.abs();
+            dist <= nanobot.r
+        })
+        .count()
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -40,13 +87,11 @@ struct NanoBot {
     r: i32,
 }
 
-impl NanoBot {
-    fn range_contains(&self, other: &Self) -> bool {
-        let dx = (self.x - other.x).abs();
-        let dy = (self.y - other.y).abs();
-        let dz = (self.z - other.z).abs();
-        dx + dy + dz <= self.r
-    }
+fn parse_input(path: &str) -> Vec<NanoBot> {
+    let mut buf = String::new();
+    let mut file = File::open(path).unwrap();
+    file.read_to_string(&mut buf).unwrap();
+    buf.lines().map(|line| line.parse().unwrap()).collect()
 }
 
 impl FromStr for NanoBot {
