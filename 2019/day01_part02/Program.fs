4b465ac8aef64afd0874f7b17097d22393056351
bpglaser <bpglaser@gmail.com>
2019-12-01 08:54:48 -0500

    Day 01 part 02 complete

---
+++ 2019/day01_part02/Program.fs
@@ -0,0 +1,16 @@
+﻿open System.IO
+
+let rec getFuelRequired n =
+    let fuel = (n / 3) - 2
+    if fuel <= 0 then
+        0
+    else
+        fuel + getFuelRequired fuel
+
+[<EntryPoint>]
+let main argv =
+    argv.[0]
+    |> File.ReadLines
+    |> Seq.sumBy (int >> getFuelRequired)
+    |> printfn "%i"
+    0
