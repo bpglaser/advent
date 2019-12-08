43b0e6a6009984803814aad48439def80794563e
bpglaser <bpglaser@gmail.com>
2019-12-08 09:10:42 -0500

    Day 08 part 01 complete

---
+++ 2019/day08_part01/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+let solve (width, height) (s: int seq) =
+    let bestLayer = 
+        s
+        |> Seq.chunkBySize (width * height)
+        |> Seq.map (Seq.countBy id)
+        |> Seq.minBy (Seq.tryFind (fst >> ((=) 0)) >> Option.map snd >> Option.defaultValue 0)
+        |> Map.ofSeq
+    (Map.find 1 bestLayer) * (Map.find 2 bestLayer)
+
+[<EntryPoint>]
+let main argv =
+    argv.[0]
+    |> File.ReadLines
+    |> Seq.head
+    |> Seq.map (string >> Int32.Parse)
+    |> solve (25, 6)
+    |> printfn "%i"
+    0
