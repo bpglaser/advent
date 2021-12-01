5f752dffe4d632046803651fa67b4085d5855887
bpglaser <bpglaser@gmail.com>
2021-11-30 21:35:34 -0800

    Day 01 part 01 complete

---
+++ 2021/day01_part01/src/main.rs
@@ -0,0 +1,22 @@
+use std::env::args;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = args().skip(1).next().ok_or("not enough args")?;
+    let mut f = File::open(path)?;
+    let mut buf = vec![];
+    f.read_to_end(&mut buf)?;
+    let mut values = vec![];
+    for line in buf.lines() {
+        let line = line?;
+        let value: i32 = line.parse()?;
+        values.push(value);
+    }
+    let answer = values
+        .windows(2)
+        .fold(0, |acc, xs| if xs[0] < xs[1] { acc + 1 } else { acc });
+    println!("{:?}", answer);
+    Ok(())
+}
