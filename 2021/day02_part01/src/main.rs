fa21ad22212d8c522589c7e54fec9cbdc3cc58e5
bpglaser <bpglaser@gmail.com>
2021-12-01 21:21:12 -0800

    Day 02 part 01 complete

---
+++ 2021/day02_part01/src/main.rs
@@ -0,0 +1,26 @@
+use std::env::args;
+use std::error::Error;
+use std::fs::read_to_string;
+use std::panic;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = args().skip(1).next().ok_or("not enough args")?;
+    let content = read_to_string(&path)?;
+    let (x, y) = content.lines().map(parse_line).fold((0, 0), do_move);
+    println!("{}", x * y);
+    Ok(())
+}
+
+fn parse_line(line: &str) -> (&str, i32) {
+    let i = line.find(' ').expect("no space in line");
+    (&line[0..i], line[i + 1..].parse().expect("invalid number in line"))
+}
+
+fn do_move((x, y): (i32, i32), (action, n): (&str, i32)) -> (i32, i32) {
+    match action {
+        "forward" => (x + n, y),
+        "down" => (x, y + n),
+        "up" => (x, y - n),
+        _ => panic!("unknown action: {}", action),
+    }
+}
