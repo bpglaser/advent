90200503684333c35d0043f045a043aa7a58a781
bpglaser <bpglaser@gmail.com>
2021-12-25 09:50:29 -0800

    Day 05 part 02 complete

---
+++ 2021/day05_part02/Program.fs
@@ -0,0 +1,78 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+open System.Text
+
+type Point =
+    { X: int
+      Y: int }
+
+type Line =
+    { P1: Point
+      P2: Point }
+
+type Grid = Map<Point, int>
+
+let pattern = Regex "(\d+),(\d+) -> (\d+),(\d+)"
+
+let parse line =
+    let m = pattern.Match line
+    let nums =
+        m.Groups
+        |> Seq.skip 1
+        |> Seq.map (fun group -> group.Value |> int)
+        |> Array.ofSeq
+    { P1 = { X = nums[0]; Y = nums[1] }
+      P2 = { X = nums[2]; Y = nums[3] } }
+
+let isHorizontal line = line.P1.Y = line.P2.Y
+
+let isVertical line = line.P1.X = line.P2.X
+
+let range a b =
+    seq { if a < b then for i in a .. b -> i else for i in a .. -1 .. b -> i }
+
+let intermediaryPoints line : Point seq =
+    if isHorizontal line || isVertical line then
+        seq {
+            for y in range line.P1.Y line.P2.Y do
+                for x in range line.P1.X line.P2.X do
+                    yield { X = x; Y = y }
+        }
+    else
+        seq {
+            for (x, y) in Seq.zip (range line.P1.X line.P2.X) (range line.P1.Y line.P2.Y) ->
+                { X = x; Y = y }
+        }
+
+let fill grid line =
+    line
+    |> intermediaryPoints
+    |> Seq.fold (fun grid p ->
+        Map.change p (fun count ->
+            match count with
+            | Some count -> Some(count + 1)
+            | None -> Some(1)) grid) grid
+
+let lineToString line =
+    sprintf "%d,%d -> %d,%d" line.P1.X line.P1.Y line.P2.X line.P2.Y
+
+let gridToString (grid: Grid) =
+    let keys = grid |> Map.keys
+    let (minX, minY) = (keys |> Seq.map (fun p -> p.X) |> Seq.min, keys |> Seq.map (fun p -> p.Y) |> Seq.min)
+    let (maxX, maxY) = (keys |> Seq.map (fun p -> p.X) |> Seq.max, keys |> Seq.map (fun p -> p.Y) |> Seq.max)
+    let sb = StringBuilder()
+    for y in minY .. maxY do
+        for x in minX .. maxX do
+            sb.Append(grid |> Map.tryFind { X = x; Y = y } |> Option.map string |> Option.defaultValue ".") |> ignore
+        sb.AppendLine() |> ignore
+    sb.ToString()
+
+
+Environment.GetCommandLineArgs()[1]
+|> File.ReadAllLines
+|> Array.map parse
+|> Array.fold fill Map.empty
+|> Seq.filter (fun kv -> kv.Value > 1)
+|> Seq.length
+|> printfn "%i"
---
+++ 2021/day05_part02/tests/Program.fs
@@ -0,0 +1,62 @@
+﻿open Expecto
+open Program
+
+[<Tests>]
+let tests = testList "intermediaryPoints" [
+    let testCases = [
+        {|
+            Name = "vert"
+            Points = (1, 1, 1, 3)
+            Expected = [
+                (1, 1)
+                (1, 2)
+                (1, 3)
+            ]
+        |}
+        {|
+            Name = "horiz"
+            Points = (9, 7, 7, 7)
+            Expected = [
+                (9, 7)
+                (8, 7)
+                (7, 7)
+            ]
+        |}
+        {|
+            Name = "rising"
+            Points = (1, 1, 3, 3)
+            Expected = [
+                (1, 1)
+                (2, 2)
+                (3, 3)
+            ]
+        |}
+        {|
+            Name = "backwards"
+            Points = (9, 7, 7, 9)
+            Expected = [
+                (9, 7)
+                (8, 8)
+                (7, 9)
+            ]
+        |}
+    ]
+    for tc in testCases -> test tc.Name {
+        let (x1, y1, x2, y2) = tc.Points
+        let p1 = { X = x1; Y = y1 }
+        let p2 = { X = x2; Y = y2 }
+        let actual =
+            { P1 = p1; P2 = p2 }
+            |> intermediaryPoints
+            |> List.ofSeq
+        let expected = [
+            for (x, y) in tc.Expected ->
+                { X = x; Y = y}
+        ]
+        Expect.equal actual expected "points equal"
+    }
+]
+
+[<EntryPoint>]
+let main argv =
+    runTestsInAssemblyWithCLIArgs [] argv
