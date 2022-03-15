977df256f96156620f62890a236d2d001eb492fb
bpglaser <bpglaser@gmail.com>
2022-03-14 17:17:49 -0700

    Day 06 part 02 complete

---
+++ 2021/day06_part02/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open System.IO
+
+let parse (line: string) =
+    line.Split ","
+    |> Seq.map int
+    |> Seq.countBy id
+    |> Seq.map (fun (a, b) -> a, bigint b)
+    |> Map.ofSeq
+
+let inc v entry =
+    match entry with
+    | Some i -> Some(bigint.Add(i, v))
+    | None -> Some v
+
+let rec solve count fishes =
+    if count = 0 then
+        fishes |> Map.values |> Seq.sum
+    else
+        let next fishes k v =
+            if k = 0 then
+                fishes
+                |> Map.change 6 (inc v)
+                |> Map.change 8 (inc v)
+            else
+                fishes |> Map.change (k - 1) (inc v)
+
+        let fishes = fishes |> Map.fold next Map.empty
+        solve (count - 1) fishes
+
+Environment.GetCommandLineArgs()[1]
+|> File.ReadAllLines
+|> Array.head
+|> parse
+|> solve 256
+|> printfn "%A"
