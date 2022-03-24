8052e9f79d193d50300fad2bce98c5c48cc76773
bpglaser <bpglaser@gmail.com>
2022-03-23 18:42:59 -0700

    Revert "Revert "Day 08 part 01 complete""
    This reverts commit 2db74d83bf14fe63adf1c0da0a4cc9608b1973a7.

---
+++ 2021/day08_part01/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.IO
+
+type Input =
+    { Patterns: string array
+      Output: string array }
+
+let guessDigit s =
+    match String.length s with
+    | 2 -> Some 1
+    | 4 -> Some 4
+    | 3 -> Some 7
+    | 7 -> Some 8
+    | _ -> None
+
+let parse (line: string) =
+    match line.Split('|') with
+    | [| patterns; output |] ->
+        { Patterns = patterns.Split(' ')
+          Output = output.Split(' ') }
+    | _ -> failwithf "invalid line: %s" line
+
+let solve inputs =
+    inputs
+    |> Seq.sumBy (fun input ->
+        input.Output
+        |> Seq.filter (guessDigit >> Option.isSome)
+        |> Seq.length)
+
+[<EntryPoint>]
+let main args =
+    args[0]
+    |> File.ReadAllLines
+    |> Seq.map parse
+    |> solve
+    |> printfn "%A"
+
+    0
