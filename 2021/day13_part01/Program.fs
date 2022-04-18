dc7092d3d40a9fb380304faa6e9281fa868c0f22
bpglaser <bpglaser@gmail.com>
2022-04-18 14:14:37 -0700

    Day 13 part 01 WIP

---
+++ 2021/day13_part01/Program.fs
@@ -0,0 +1,46 @@
+﻿open System
+open System.IO
+
+type Fold =
+    | XFold of int
+    | YFold of int
+
+let xPrefix = "fold along x="
+let yPrefix = "fold along y="
+
+let parse lines =
+    let pair (s: string) =
+        match s.Split(',', StringSplitOptions.TrimEntries) with
+        | [| x; y |] -> Some(int x, int y)
+        | _ -> None
+
+    let fold (s: string) =
+        if s.StartsWith xPrefix then
+            s.Substring xPrefix.Length |> int |> XFold |> Some
+        elif s.StartsWith yPrefix then
+            s.Substring yPrefix.Length |> int |> YFold |> Some
+        else
+            None
+
+    Array.choose pair lines, Array.choose fold lines
+
+let applyFold fold points =
+    match fold with
+    | XFold i ->
+        points
+    | YFold i ->
+        points
+
+let solve (points, folds) =
+    applyFold (Array.head folds) points
+    |> Array.length
+
+[<EntryPoint>]
+let main args =
+    args[0]
+    |> File.ReadAllLines
+    |> parse
+    |> solve
+    |> printfn "%A"
+
+    0
